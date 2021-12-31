4862ce5ea382df1f79f8888a2d7ebcae31f11320
“Stoeff25” <christoph.tiger@hotmail.com>
2021-12-31 10:53:01 +0000

    modified:   hello-world/src/App.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import React from "react";
 
-
- const App =  () => {
-
-  const getData = async() => {
+const App = () => {
+  const getData = async () => {
     try {
       const response = await axios({
         url: `http://65.21.60.19:4000/getAll`,
@@ -14,21 +12,19 @@ import React from "react";
           "Content-type": "application/json",
         },
       });
-     
+
       console.log(response);
     } catch (err) {
-      console.log("error", err)
+      console.log("error", err);
     }
   };
-  
-
 
   return (
     <div>
-    <h1>Hello World React Client</h1>
-    <button onClick={getData}>Test</button>
+      <h1>Hello World React Client</h1>
+      <button onClick={getData}>Test Click</button>
     </div>
   );
-}
+};
 
 export default App;
